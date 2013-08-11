74462a02368703f8bae91e81bfc569c54dce8b18
latrop7 <latrop7@live.com>
2013-08-11 23:39:12 +1000

    Fix CM Inline Building

--- Android.mk
+++ Android.mk
@@ -323,12 +323,6 @@ endif
 ifeq ($(BUILD_ID), GINGERBREAD)
     TW_NO_EXFAT := true
 endif
-ifneq ($(TW_NO_EXFAT), true)
-    include $(commands_recovery_local_path)/exfat/exfat-fuse/Android.mk \
-            $(commands_recovery_local_path)/exfat/mkfs/Android.mk \
-            $(commands_recovery_local_path)/fuse/Android.mk \
-            $(commands_recovery_local_path)/exfat/libexfat/Android.mk
-endif
 ifeq ($(TW_INCLUDE_CRYPTO), true)
     include $(commands_recovery_local_path)/crypto/ics/Android.mk
 endif
